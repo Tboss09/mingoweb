447669f774647f2de88002149ed8ba40d9a35521
Akosile Tayo <akosilety9@gmail.com>
2021-02-13 03:29:46 +0100

    Made the Images More Responsive

--- script/main.js
+++ script/main.js
@@ -49,6 +49,10 @@ var x = setInterval(function () {
 
 // AutoPlay Gallery
 
+$(window).on('resize orientationchange', function () {
+    $('.galleryImages__imgContainer').slick('resize');
+});
+
 
 // SLDER BEGUN
 $('.galleryImages__imgContainer').slick({
@@ -60,6 +64,32 @@ $('.galleryImages__imgContainer').slick({
     initialSlide: 0,
     infinite: true,
     prevArrow: '<button type = "button" class="slick-prev slick__btn">Previous</button>',
-    nextArrow: '<button type = "button" class="slick-next slick__btn">Previous</button>'
+    nextArrow: '<button type = "button" class="slick-next slick__btn">Previous</button>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
 });
-
